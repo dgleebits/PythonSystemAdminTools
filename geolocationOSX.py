0097c570bf4a3e7979594942bf5b22275d3c5e30
bigsnarfdude <ohprecio@gmail.com>
2012-10-04 12:20:36 -0400

    refactored code into functions and to use xml

--- geolocationOSX.py
+++ geolocationOSX.py
@@ -3,79 +3,79 @@
 # modified to english version by Dan Gleebits 20/06/2012
 # modified to run on OS X by James Armitage 25/06/2012
 # modified to process in python Dan Gleebits 26/06/2012
-# commented all the file for Dan. Vincent Ohprecio 01/10/2012
+# parsing xml Vincent Ohprecio 01/10/2012
 
-# import all the necessary libraries
 from commands import getoutput
 import re, urllib2, webbrowser
 import json as simplejson
 import xml.etree.ElementTree as ET
 
+airport_scan_xml = '/System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport --scan -x'
+macMatch = '([a-fA-F0-9]{1,2}[:|\-]?){6}'
 
-# bash command to grab the neighboring wifi data around the laptop
-path2WiFi = '/System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport --scan -x'
+def get_bssid_signal_strength():
+    signal_by_address = {}
+    root = ET.fromstring(getoutput(airport_scan_xml))
 
-# regular expression to match MAC address
-#macMatch = '([a-fA-F0-9]{2}[:|\-]?){6}'
-#count = 0
+    for level_1 in root:
+        for level_2 in level_1:
+            for level_3 in level_2.findall('string'):
+                if re.compile(macMatch).search(level_3.text):
+                    mac_address = level_3.text
+            for level_3 in level_2:
+                if level_3.text == None:
+                    pass
+                else:
+                    if len(level_3.text) < 6 and len(level_3.text) > 1:
+                        if "NOISE" in level_3.text:
+                            flag = 2
+                        if "RSSI" in level_3.text:
+                            flag = 1
+                        if len(level_3.text) < 4:
+                            try:
+                                signal_number = int(level_3.text)
+                                if signal_number < 0:
+                                    final_signal_strength = abs(int(level_3.text))
+                                if flag == 1:
+                                    signal_strength = final_signal_strength
+                            except:
+                                pass
+                        if "SSID" == level_3.text:
+                            flag = 0
+            signal_by_address[mac_address] = signal_strength
 
-# running network WiFi scan
-print "[+] Scanning network"
-neighborWiFi = getoutput(path2WiFi)
-#neighborWiFi = neighborWiFi.split('\n')
+    return signal_by_address
 
-# cleaning up bad data		
-#for line in neighborWiFi:
-#	a = re.compile(macMatch).search(line)
-#	if a:
-#		count +=1
-#	else:
-#		neighborWiFi.pop(count)
+def convert_dict_to_json(signal_by_address):
+    location_request = {
+        "version": "1.1.0",
+        "request_address": False,
+        "wifi_towers": [],
+        }
 
-# using xml parsing in ElementTree to find each BSSID 
-#<key>BSSID</key>
-#<string>0:17:c5:be:e0:18</string>
-#<key>NOISE</key>
-#<integer>-92</integer>
+    for address, signal in signal_by_address.items():
+        tower = {"mac_address": address, "signal_strength": signal}
+        location_request["wifi_towers"].append(tower)
 
+    return simplejson.JSONEncoder().encode(location_request)
 
-tree = ET.parse('../wifi.xml')
-root = tree.getroot()
+def post_json_and_get_lat_long(json):
+    output = simplejson.loads(urllib2.urlopen('https://www.google.com/loc/json', json).read())
 
-print type(root)
-print root
+    return output["location"]["latitude"], output["location"]["longitude"]
 
 
+if __name__ == "__main__":
+    print "[+] Scanning network"
+    signal_by_address = get_bssid_signal_strength()
 
+    json = convert_dict_to_json(signal_by_address)
 
+    print "[+] Sending the request to Google"
+    loc = post_json_and_get_lat_long(json)
 
+    map_url = "http://maps.google.com/maps?q=%s,%s" % loc
+    print "[+] Google Map"
+    print map_url
 
-		
-print "[+] Creating HTML request"
-locationRequest={
-		"version":"1.1.0",
-		"request_address":False, 
-		"wifi_towers":[{"mac_address":"00-00-00-00-00-00","signal_strength":0}],
-		}
-
-# this is cleaning up the data file splitting the data into MAC Address and Signal Strength				
-#for x in neighborWiFi:
-#    a = re.compile(macMatch).search(x.split()[1])
-#    b = re.compile(macMatch).search(x.split()[2])
-#    if a:
-#        tempDict = {"mac_address":x.split()[1].replace(":","-"),"signal_strength":abs(int(x.split()[2]))}
-#        locationRequest["wifi_towers"].append(tempDict)
-#    elif b:
-#        tempDict = {"mac_address":x.split()[2].replace(":","-"),"signal_strength":abs(int(x.split()[3]))}
-#        locationRequest["wifi_towers"].append(tempDict)
-           
-# this takes the cleaned up data and serialize to JSON request for Google API
-print "[+] Sending the request to Google"
-#data = simplejson.JSONEncoder().encode(locationRequest)
-#output = simplejson.loads(urllib2.urlopen('https://www.google.com/loc/json', data).read())
-
-# prints out the latitude and longitute data returned from Google and opens browser to visually location MAC
-print "[+] Google Map"
-#print "http://maps.google.com/maps?q="+str(output["location"]["latitude"])+","+str(output["location"]["longitude"])
-#googleMapWebpage = "http://maps.google.com/maps?q="+str(output["location"]["latitude"])+","+str(output["location"]["longitude"])
-#webbrowser.open(googleMapWebpage)
+    webbrowser.open(map_url)
